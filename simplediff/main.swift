96043705c765deac1fa3f20a3f4a949d9a7c5d94
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2015-03-31 14:57:41 +0200

    Initial Commit

---
+++ simplediff/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  simplediff
+//
+//  Created by Matthias Hochgatterer on 31/03/15.
+//  Copyright (c) 2015 Matthias Hochgatterer. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
