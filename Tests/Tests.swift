e2ff0df870766dd3ea6543557d79020be7776cf0
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2016-09-22 16:37:25 +0200

    Rename Operation to Change and update syntax for Swift 3

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -6,52 +6,53 @@
 //  Copyright (c) 2015 Matthias Hochgatterer. All rights reserved.
 //
 
-import Cocoa
+import Swift
 import XCTest
 
 class Tests: XCTestCase {
     func testCharacterdiff() {
-        let result = diff([Character]("Matthias"), [Character]("Matthew"))
+        let toCharacters: ((String) -> [String]) = { $0.characters.map({ "\($0)" }) }        
+        let result = simplediff(before: toCharacters("Matthias"), after: toCharacters("Matthew"))
         XCTAssertEqual(result.count, 3)
-        XCTAssertEqual(result[0].type, .Noop)
-        XCTAssertEqual(result[0].elements, [Character]("Matth"))
-        XCTAssertEqual(result[1].type, .Delete)
-        XCTAssertEqual(result[1].elements, [Character]("ias"))
-        XCTAssertEqual(result[2].type, .Insert)
-        XCTAssertEqual(result[2].elements, [Character]("ew"))
+        XCTAssertEqual(result[0].type, .noop)
+        XCTAssertEqual(result[0].elements, toCharacters("Matth"))
+        XCTAssertEqual(result[1].type, .delete)
+        XCTAssertEqual(result[1].elements, toCharacters("ias"))
+        XCTAssertEqual(result[2].type, .insert)
+        XCTAssertEqual(result[2].elements, toCharacters("ew"))
     }
     
     func testWordsdiff() {
-        let before = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.".componentsSeparatedByString(" ")
-        let after = "Lorem ipsum dolor amet, consectetur sit amet elit.".componentsSeparatedByString(" ")
-        let result = diff(before, after)
+        let before = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.".components(separatedBy: " ")
+        let after = "Lorem ipsum dolor amet, consectetur sit amet elit.".components(separatedBy: " ")
+        let result = simplediff(before: before, after: after)
         XCTAssertEqual(result.count, 6)
-        XCTAssertEqual(result[0].type, .Noop)
-        XCTAssertEqual(result[0].elements, "Lorem ipsum dolor".componentsSeparatedByString(" "))
-        XCTAssertEqual(result[1].type, .Delete)
+        XCTAssertEqual(result[0].type, .noop)
+        XCTAssertEqual(result[0].elements, "Lorem ipsum dolor".components(separatedBy: " "))
+        XCTAssertEqual(result[1].type, .delete)
         XCTAssertEqual(result[1].elements, ["sit"])
-        XCTAssertEqual(result[2].type, .Noop)
-        XCTAssertEqual(result[2].elements, "amet, consectetur".componentsSeparatedByString(" "))
-        XCTAssertEqual(result[3].type, .Delete)
+        XCTAssertEqual(result[2].type, .noop)
+        XCTAssertEqual(result[2].elements, "amet, consectetur".components(separatedBy: " "))
+        XCTAssertEqual(result[3].type, .delete)
         XCTAssertEqual(result[3].elements, ["adipiscing"])
-        XCTAssertEqual(result[4].type, .Insert)
-        XCTAssertEqual(result[4].elements, "sit amet".componentsSeparatedByString(" "))
-        XCTAssertEqual(result[5].type, .Noop)
+        XCTAssertEqual(result[4].type, .insert)
+        XCTAssertEqual(result[4].elements, "sit amet".components(separatedBy: " "))
+        XCTAssertEqual(result[5].type, .noop)
         XCTAssertEqual(result[5].elements, ["elit."])
     }
     
     func testNumbers() {
-        let result = diff([1, 4, 10], [1, 2, 3, 4, 5])
+        let result = simplediff(before: [1, 4, 10], after: [1, 2, 3, 4, 5])
         XCTAssertEqual(result.count, 5)
-        XCTAssertEqual(result[0].type, .Noop)
+        XCTAssertEqual(result[0].type, .noop)
         XCTAssertEqual(result[0].elements, [1])
-        XCTAssertEqual(result[1].type, .Insert)
+        XCTAssertEqual(result[1].type, .insert)
         XCTAssertEqual(result[1].elements, [2, 3])
-        XCTAssertEqual(result[2].type, .Noop)
+        XCTAssertEqual(result[2].type, .noop)
         XCTAssertEqual(result[2].elements, [4])
-        XCTAssertEqual(result[3].type, .Delete)
+        XCTAssertEqual(result[3].type, .delete)
         XCTAssertEqual(result[3].elements, [10])
-        XCTAssertEqual(result[4].type, .Insert)
+        XCTAssertEqual(result[4].type, .insert)
         XCTAssertEqual(result[4].elements, [5])
     }
 }
--- simplediff/simplediff.swift
+++ simplediff/simplediff.swift
@@ -8,35 +8,35 @@
 
 import Foundation
 
-enum OperationType {
-    case Insert, Delete, Noop
+enum ChangeType {
+    case insert, delete, noop
     
     var description: String {
         get {
             switch self {
-            case .Insert: return "+"
-            case .Delete: return "-"
-            case .Noop: return "="
+            case .insert: return "+"
+            case .delete: return "-"
+            case .noop: return "="
             }
         }
     }
 }
 
 /// Operation describes an operation (insertion, deletion, or noop) of elements.
-struct Operation<T> {
-    let type: OperationType
+struct Change<T> {
+    let type: ChangeType
     let elements: [T]
     
     var elementsString: String {
-        return "".join(elements.map { "\($0)" })
+        return elements.map({ "\($0)" }).joined(separator: "")
     }
     
     var description: String {
         get {
             switch type {
-            case .Insert:
+            case .insert:
                 return "[+\(elementsString)]"
-            case .Delete:
+            case .delete:
                 return "[-\(elementsString)]"
             default:
                 return "\(elementsString)"
@@ -48,14 +48,14 @@ struct Operation<T> {
 /// diff finds the difference between two lists.
 /// This algorithm a shameless copy of simplediff https://github.com/paulgb/simplediff
 ///
-/// :param: before Old list of elements.
-/// :param: after New list of elements
-/// :returns: A list of operation (insert, delete, noop) to transform the list *before* to the list *after*.
-func diff<T where T: Equatable, T: Hashable>(before: [T], after: [T]) -> [Operation<T>] {
+/// - parameter before: Old list of elements.
+/// - parameter after: New list of elements
+/// - returns: A list of operation (insert, delete, noop) to transform the list *before* to the list *after*.
+func simplediff<T>(before: [T], after: [T]) -> [Change<T>] where T: Hashable {
     // Create map of indices for every element
     var beforeIndices = [T: [Int]]()
-    for (index, elem) in enumerate(before) {
-        var indices = beforeIndices.indexForKey(elem) != nil ? beforeIndices[elem]! : [Int]()
+    for (index, elem) in before.enumerated() {
+        var indices = beforeIndices.index(forKey: elem) != nil ? beforeIndices[elem]! : [Int]()
         indices.append(index)
         beforeIndices[elem] = indices
     }
@@ -64,9 +64,9 @@ func diff<T where T: Equatable, T: Hashable>(before: [T], after: [T]) -> [Operat
     var afterStart = 0
     var maxOverlayLength = 0
     var overlay = [Int: Int]() // remembers *overlayLength* of previous element
-    for (index, elem) in enumerate(after) {
+    for (index, elem) in after.enumerated() {
         var _overlay = [Int: Int]()
-         // Element must be in *before* list
+        // Element must be in *before* list
         if let elemIndices = beforeIndices[elem] {
             // Iterate over element indices in *before*
             for elemIndex in elemIndices {
@@ -85,22 +85,22 @@ func diff<T where T: Equatable, T: Hashable>(before: [T], after: [T]) -> [Operat
         overlay = _overlay
     }
     
-    var operations = [Operation<T>]()
+    var operations = [Change<T>]()
     if maxOverlayLength == 0 {
-         // No overlay; remove before and add after elements
+        // No overlay; remove before and add after elements
         if before.count > 0 {
-            operations.append(Operation(type: .Delete, elements: before))
+            operations.append(Change(type: .delete, elements: before))
         }
         if after.count > 0 {
-            operations.append(Operation(type: .Insert, elements: after))
+            operations.append(Change(type: .insert, elements: after))
         }
     } else {
         // Recursive call with elements before overlay
-        operations += diff(Array(before[0..<beforeStart]), Array(after[0..<afterStart]))
+        operations += simplediff(before: Array(before[0..<beforeStart]), after: Array(after[0..<afterStart]))
         // Noop for longest overlay
-        operations.append(Operation(type: .Noop, elements: Array(after[afterStart..<afterStart+maxOverlayLength])))
+        operations.append(Change(type: .noop, elements: Array(after[afterStart..<afterStart+maxOverlayLength])))
         // Recursive call with elements after overlay
-        operations += diff(Array(before[beforeStart+maxOverlayLength..<before.count]), Array(after[afterStart+maxOverlayLength..<after.count]))
+        operations += simplediff(before: Array(before[beforeStart+maxOverlayLength..<before.count]), after: Array(after[afterStart+maxOverlayLength..<after.count]))
     }
     return operations
 }
